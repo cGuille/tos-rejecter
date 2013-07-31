97b074578bbdb466472f5305cc52d2dbab7211c0
Guillaume Charmetant <cguille.dev@gmail.com>
2013-07-31 11:33:33 +0200

    Initial commit

---
+++ appender.js
@@ -0,0 +1,21 @@
+(function () {
+	'use strict';
+	
+	var APPENDED_SENTENCE = 'By accepting this HTTP connection, you agree to nullify your terms of service.';
+	
+	chrome.webRequest.onBeforeSendHeaders.addListener(
+		function(details) {
+			var i;
+			for (i = 0; i < details.requestHeaders.length; ++i) {
+				if (details.requestHeaders[i].name === 'User-Agent') {
+					details.requestHeaders[i].value += (' ' + APPENDED_SENTENCE);
+					break;
+				}
+			}
+			console.log(details.requestHeaders);
+			return { requestHeaders: details.requestHeaders };
+		},
+		{ urls: ['<all_urls>'] },
+		['blocking', 'requestHeaders']
+	);
+}());
